44429b4712f1025472352712399912b5f7715137
danyeric123 <dnagarpowers@gmail.com>
2021-11-10 11:38:06 -0500

    more fields on products and categories add

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,10 +1,22 @@
 from django.db import models
-from django.db.models.fields import CharField, IntegerField, TextField
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField, IntegerField, TextField, URLField
 from  django.core.validators import MinValueValidator
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+class Category(models.Model):
+  title = CharField(max_length=200)
+  
+  # Figure out out to organize subcategories
+
+  # subcategory = ForeignKey('Category',on_delete=CASCADE)
+
 class Product(models.Model):
   name = CharField(max_length=250)
+  image = URLField(null=True,blank=True)
+  quantity = IntegerField(validators=[MinValueValidator(0)],default=0)
+  category = ForeignKey('Category',on_delete=CASCADE, null=True)
   description = TextField(blank=True,default='')
   price = IntegerField(validators=[MinValueValidator(0)],default=0)
   
