992e918b1629583ea2cdb51ae6f185f2143ea51d
danyeric123 <dnagarpowers@gmail.com>
2021-11-15 15:35:21 -0500

    better getstaticpaths function

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    path('api/products/', include('products.urls'))
+    path('api/', include('products.urls'))
 ]
